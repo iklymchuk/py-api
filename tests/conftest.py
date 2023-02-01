2a14eaa022d2b14a96f046b4324ec5ce0559a7d4
Ivan Klymchuk <klymhuk.ivan@gmail.com>
2023-02-01 20:32:17 +0000

    refactor a project structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,22 +8,15 @@ def unique_user_data():
     payload = file_reader.read_file("create_user.json")
     unique_username = f"User {str(uuid4())}"
     payload["username"] = unique_username
-
-    # yield payload
-    return payload
-
+    yield payload
 
 @pytest.fixture
 def updated_user_data():
     payload = file_reader.read_file("update_user.json")
-
-    # yield payload
-    return payload
+    yield payload
 
 
 @pytest.fixture
 def user_schema():
     payload = file_reader.read_file("user_schema.json")
-
-    # yield payload
-    return payload
+    yield payload
